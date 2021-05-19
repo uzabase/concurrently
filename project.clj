06a83a68b251f2a69ac616cc4340b4421dfff9d4
Tsutomu YANO <t_yano@me.com>
2021-05-19 17:45:18 +0900

    version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject concurrently "0.2.0"
+(defproject concurrently "0.2.1"
   :description "A clojure library for running actions concurrently but restricting a number of threads."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
