81765710d680c588c9beca794ae7d66d151859a7
Tsutomu YANO <tsutomuyano@shelfinc.com>
2021-10-21 19:15:34 +0900

    Upgraded the version of core.async

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/core.async "1.3.618"]
+                 [org.clojure/core.async "1.3.622"]
                  [org.clojure/tools.logging "1.1.0"]
                  [databox "0.1.7"]]
   :profiles {:dev {:dependencies [[eftest "0.5.9"]
