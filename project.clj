9db3d6ccbe0a64e615e4bcb5218b7c67660a28a8
Tsutomu YANO <t_yano@me.com>
2022-10-13 14:31:30 +0900

    Using databox 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.3.622"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [databox "0.1.7"]]
+                 [databox "0.2.1-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[eftest "0.5.9"]
                                   [com.taoensso/timbre "5.1.2"]
                                   [com.fzakaria/slf4j-timbre "0.3.21"]]
